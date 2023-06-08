8f130917f7bb1edecc1797f5f13556cf2407d782
hanni <pyaesone.hanni@gmail.com>
2023-06-08 15:58:38 +0630

    Finished Tutorial3

---
+++ src/main/java/tutorial/Tutorial3C.java
@@ -0,0 +1,20 @@
+package main.java.tutorial;
+
+import java.util.ArrayList;
+
+public class Tutorial3C {
+
+	public static void main(String[] args) {
+		ArrayList<String> alist = new ArrayList<>();
+		alist.add("Red");
+		alist.add("Green");
+		alist.add("Orange");
+		alist.add("White");
+
+		alist.add("Black");
+		System.out.println("ArrayList : " + alist);
+
+			}
+	}
+
+
