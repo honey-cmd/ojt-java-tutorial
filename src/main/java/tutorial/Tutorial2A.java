493297cc5af952a9ec9eefdc72afde3127d9d88f
hanni <pyaesone.hanni@gmail.com>
2023-06-08 15:47:03 +0630

    Finished Tutorial2

--- src/main/java/tutorial/Tutorial2A.java
+++ src/main/java/tutorial/Tutorial2A.java
@@ -1,10 +1,22 @@
 package main.java.tutorial;
 
+import java.util.Scanner;
+
 public class Tutorial2A {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num;
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter One Number : ");
+		num = sc.nextInt();
+
+		if (num >= 0) {
+			System.out.println(num + "is a positive number");
+
+		} else {
+			System.out.println(num + " is a negative number");
+		}
 
 	}
-
 }
