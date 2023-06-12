acd461d109576bbcafe136dc10a671c3030c6c0f
hanni <pyaesone.hanni@gmail.com>
2023-06-12 20:44:01 +0630

    use regex pattern

--- src/main/java/tutorial/Tutorial6.java
+++ src/main/java/tutorial/Tutorial6.java
@@ -7,53 +7,39 @@ import java.util.regex.Pattern;
 public class Tutorial6 {
 
 	public static void main(String[] args) {
+
 		String password;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a password");
 		password = sc.nextLine();
 
-		if (strongPassword(password)) {
-			System.out.println("Password is strong");
+		if (strongPassword(password) == true) {
+			System.out.println("Your password is strong");
+
 		} else {
-			System.out.println("Password is not strong");
+			System.out.println("Your password is not strong");
 		}
 
 	}
 
 	public static boolean strongPassword(String password) {
+
 		if (password.length() < 8) {
 			return false;
+
 		}
-		boolean hasUpperCase = false;
-		boolean hasLowerCase = false;
-		boolean hasNumber = false;
-		boolean hasRegx = false;
+		if (!(password.matches(password))) {
+			return false;
 
-
-		Pattern pattern = Pattern.compile(password);
-		Matcher matcher = pattern.matcher(password);
-		
-	
-		
-
-		for (char c : password.toCharArray()) {
-			if (Character.isUpperCase(c)) {
-				hasUpperCase = true;
-			} else if (Character.isLowerCase(c)) {
-				hasLowerCase = true;
-
-			} else if (Character.isDigit(c)) {
-				hasNumber = true;
-			}else if (matcher.matches()) {
-				hasRegx = true;
-			}
-			
-			if (hasUpperCase && hasLowerCase && hasNumber && hasRegx) {
-				break;
-			}
-			
 		}
-		return hasUpperCase && hasLowerCase && hasNumber && hasRegx ;
+
+		String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+		Pattern regex = Pattern.compile(pattern);
+
+		Matcher matcher = regex.matcher(password);
+
+		return matcher.matches();
 	}
 
 }
